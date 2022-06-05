a1c792af1182aa40d5bb4ec5289f8de78b574d55
Jorge Saldana <jorgefernando.saldana@gmail.com>
2022-06-05 14:17:08 -0500

    adding test for binary search, flake8 is clean

--- test_binary_search_704.py
+++ test_binary_search_704.py
@@ -28,6 +28,7 @@
 # class SolutionProblem704:
 #   search(self, nums: List[int], target: int) -> int:
 
+import random
 import unittest
 from binary_search_704 import SolutionProblem704
 
@@ -38,6 +39,10 @@ class TestSolutionProblem704(unittest.TestCase):
     def setUp(self):
 
         self.solution = SolutionProblem704()
+        self.NUMS_LENGTH_MAX = 104
+        self.NUMS_LENGTH_MIN = 1
+        self.NUMS_SMALLEST = -140
+        self.NUMS_BIGGEST = 140
 
     def test_custom(self):
         """
@@ -48,3 +53,35 @@ class TestSolutionProblem704(unittest.TestCase):
         target = 2
         expected = -1
         self.assertEqual(expected, self.solution.search(nums, target))
+
+    def test_parametrized(self, positive: bool):
+        """
+        Randomize a parametrized test to stress method under test
+        """
+        # Define number of randome subtest
+        number_of_tests = 50
+
+        for i in range(number_of_tests):
+
+            # Generate random nums list
+            nums_lenght = random.randint(
+                    self.NUMS_LENGTH_MIN, self.NUMS_LENGTH_MAX)
+            nums = random.sample(
+                population=range(self.NUMS_SMALLEST, self.NUMS_BIGGEST),
+                k=nums_lenght)
+
+            # Make sure nums is sorted
+            nums.sort()
+
+            # Create a random integer target
+            target = int(random.random() * 300)
+
+            if target in nums:
+                expected = nums.index(target)
+            else:
+                expected = -1
+
+            with self.subTest(
+                    "Incorrect result", target=target,
+                    expected=expected):
+                self.assertEqual(expected, self.solution.search(nums, target))
